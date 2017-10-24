3053d5b67abb111913d5af79717398f3f5f4aeb2
Hallur Kárason Nielsen <hallurkn@hotmail.com>
2017-10-24 22:42:43 +0200

    changed text for impact story result

--- src/animations/led-impact-story-delay.js
+++ src/animations/led-impact-story-delay.js
@@ -12,41 +12,43 @@ const sky = document.querySelector('a-sky');
 const camera = document.querySelector('a-camera');
 const cursor = document.querySelector('#cursor');
 
-function init() {
+function init(savings) {
 	const ledImpactTextMixin = helpers.appendNewElement(assets, 'a-mixin', {
 		'id': 'ledImpactTextMixin',
 		'color': '#ffffff',
-		'align': 'center',
 		'baseline': 'bottom',
 		'line-height': '80',
-		'scale': '8 8',
+		'scale': '10 10',
 		'rotation': '0 -43 0',
 	});
 
 	const ledImpactText = helpers.appendNewElement(sceneElement, 'a-entity', {
 		'id': 'ledImpactText',
-		'position': '-6.067 5.107 -8.522',
+		'position': '-3.477 7.161 -6.442',
 	});
 
 	const ledImpactText1 = helpers.appendNewElement(ledImpactText, 'a-text', {
 		'id': 'ledImpactText1',
 		'mixin': 'ledImpactTextMixin',
-		'position': '13.078 9.010 -12.879',
-		'value': 'want to see the environmental',
+		'position': '18.663 9.010 -7.3',
+        'align': 'center',
+		'value': 'SAVINGS',
 	});
 
 	const ledImpactText2 = helpers.appendNewElement(ledImpactText ,'a-text', {
 		'id': 'ledImpactText1',
 		'mixin': 'ledImpactTextMixin',
-		'position': '13.273 6.106 -12.517',
-		'value': 'impact you could have on the',
+		'position': '18.663 6.106 -7.3',
+        'align': 'center',
+		'value': 'You can save',
 	});
 
 	const ledImpactText3 = helpers.appendNewElement(ledImpactText, 'a-text', {
 		'id': 'ledImpactText1',
 		'mixin': 'ledImpactTextMixin',
-		'position': '14.664 3.181 -11.367',
-		'value': 'planet by choosing led?',
+		'position': '18.663 3.181 -7.3',
+        'align': 'center',
+		'value': savings.yearlySavingsPercent + '%',
 	});
 
 	setTimeout(ledImpactYesNoTextDelay, 1000);
@@ -55,14 +57,15 @@ function init() {
 		const ledImpactYesText = helpers.appendNewElement(ledImpactText, 'a-text', {
 			'id': 'ledImpactYesText',
 			'mixin': 'ledImpactTextMixin',
-			'position': '13.201 -6.33 -12.737',
-			'value': 'YES',
+			'position': '18.663 -6.33 -7.3',
+			'value': 'START',
+            'align': 'center',
 			'scale': '25 25',
 		});
 
 		const ledImpactYesPlane = helpers.appendNewElement(ledImpactText, 'a-plane', {
 			'id': 'ledImpactYesPlane',
-			'position': '17.117 -6.330 -8.851',
+			'position': '18.663 -6.33 -7.3',
 			'rotation': '0 -43 0',
 			'width': '10',
 			'height': '5',
@@ -96,7 +99,7 @@ function init() {
 
 			const ledImpactYesTextPosition = helpers.appendNewElement(ledImpactYesText, 'a-animation', {
 				'attribute': 'position',
-				'from': '13.201 -6.33 -12.737',
+				'from': '18.663 -6.33 -7.3',
 				'to': '16.235 4 -21.652',
 				'dur': '1000',
 				'ease': 'ease-out',
