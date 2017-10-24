ac9bcb22e1cdbf4de22d4d04f42664a920275b0e
Hallur Kárason Nielsen <hallurkn@hotmail.com>
2017-10-24 22:07:09 +0200

    added data

---
+++ src/data/inputHoursData.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = [
+	{ 'type': 'input', 'value': 10 },
+	{ 'type': 'input', 'value': 100 },
+	{ 'type': 'input', 'value': 1000 },
+	{ 'type': 'text', 'value': 0 },
+];
